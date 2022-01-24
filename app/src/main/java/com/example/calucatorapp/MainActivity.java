70649696f649d67906beb444df29835421444d16
motaqi <elmehdi.motaqi@cgi.com>
2022-01-24 17:11:54 +0100

    3 commit

--- app/src/main/java/com/example/calucatorapp/MainActivity.java
+++ app/src/main/java/com/example/calucatorapp/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //
     }
 }
