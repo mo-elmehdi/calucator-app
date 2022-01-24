9ea6427392f4819c5c4be1abd2fb9fefb70ee644
motaqi <elmehdi.motaqi@cgi.com>
2022-01-24 17:02:57 +0100

    second commit

--- app/src/main/java/com/example/calucatorapp/MainActivity.java
+++ app/src/main/java/com/example/calucatorapp/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.calucatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.library.Calculator;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
